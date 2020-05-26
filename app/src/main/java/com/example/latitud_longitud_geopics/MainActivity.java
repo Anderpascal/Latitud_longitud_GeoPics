cce494aac36d714c02f524203fbdbda8c4c3d538
Anderpascal <50362627+Anderpascal@users.noreply.github.com>
2020-05-26 12:01:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/latitud_longitud_geopics/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.latitud_longitud_geopics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

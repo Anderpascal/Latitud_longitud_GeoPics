f0310707d2ad76e7222436638f069c769ec4a129
Anderpascal <50362627+Anderpascal@users.noreply.github.com>
2020-05-27 13:43:19 +0200

    Primer intento de conectar el GPS al dispositivo.

--- app/src/main/java/com/example/latitud_longitud_geopics/MainActivity.java
+++ app/src/main/java/com/example/latitud_longitud_geopics/MainActivity.java
@@ -6,46 +6,111 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
-
-import android.view.View;
+import android.util.Log;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private LocationManager locManager;
-    private Location loc;
-    TextView mensaje1;
-    TextView mensaje2;
-    TextView mensaje3;
+    private LocationManager ubicacion;
+    TextView latitud;
+    TextView longitud;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mensaje1 = findViewById(R.id.latitud);
-        mensaje2 = findViewById(R.id.longitud);
-        mensaje3 = findViewById(R.id.textView5);
+        localizacion();
+        registrarLocalizacion();
     }
 
-    public void Conectar(View view){
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            mensaje1.setText("No se han definido los permisos necesarios.");
-            mensaje2.setText("");
+    private void localizacion() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 1000);
         }
-        try{
-            locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        latitud = findViewById(R.id.txtLatitud);
+        longitud = findViewById(R.id.txtLongitud);
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location loc = ubicacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (loc != null) {
+            latitud.setText("Latitud: " + loc.getLatitude());
+            longitud.setText("Longitud: " + loc.getLongitude());
+        }
 
-            mensaje1.setText(String.valueOf(loc.getLatitude()));
-            mensaje2.setText(String.valueOf(loc.getLongitude()));
+    }
 
-        } catch (NullPointerException e) {
-            mensaje3.setText("Error");
+    private void registrarLocalizacion() {
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        ubicacion.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, new miLocalizacionListener());
+    }
+
+    private class miLocalizacionListener implements LocationListener{
+
+        @Override
+        public void onLocationChanged(Location location) {
+            System.out.println("La direccion ha cambiado");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
         }
     }
+//
+//    private void listaProvider() {
+//        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        List<String> listaProvider = ubicacion.getAllProviders();
+//
+//        String mejorProvider = ubicacion.getBestProvider(mejorCriterio(), false);
+//        System.out.println(mejorProvider);
+//
+//        LocationProvider provider = ubicacion.getProvider(listaProvider.get(0));
+//        System.out.println(provider.getAccuracy());
+//        System.out.println(provider.getPowerRequirement());
+//        System.out.println(provider.supportsAltitude());
+//    }
+
+//
+//    private Criteria mejorCriterio() {
+//        Criteria requerimiento = new Criteria();
+//        requerimiento.setAccuracy(Criteria.ACCURACY_FINE);
+//        requerimiento.setAltitudeRequired(true);
+//        return requerimiento;
+//    }
+//
+//    private boolean estadoGPS() {
+//        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        if (!ubicacion.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+//            Log.d("GPS", "NO ACTIVADO");
+//        } else {
+//            Log.d("GPS", "ACTIVADO");
+//        }
+//        return true;
+//    }
+
 
 }
